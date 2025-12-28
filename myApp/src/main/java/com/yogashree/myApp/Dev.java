19b4feffb202945e3d2509eeb5d7a7969db16eeb
YogashreeJ <yj2208@srmist.edu.in>
2025-12-28 19:53:36 +0530

    feat:Added constructor injection

--- myApp/src/main/java/com/yogashree/myApp/Dev.java
+++ myApp/src/main/java/com/yogashree/myApp/Dev.java
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Component;
 @Component  // To notify the spring that I need object of this class
 public class Dev {
 
-    @Autowired // field injection
+    // @Autowired // field injection
     Laptop laptop;
+
+    // constructor injection
+    public Dev(Laptop laptop){
+        this.laptop=laptop;
+    }
+
     public void depInject(){
         laptop.compile();
         System.out.println("This is an example of Dependency Injection");
