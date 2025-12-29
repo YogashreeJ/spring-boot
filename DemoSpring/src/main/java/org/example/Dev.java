972396db5c9d372b1ae4200f95744bd4720e1ba1
YogashreeJ <yj2208@srmist.edu.in>
2025-12-29 22:45:30 +0530

    feat:Added setter injection and constructor injection in Spring

--- DemoSpring/src/main/java/org/example/Dev.java
+++ DemoSpring/src/main/java/org/example/Dev.java
@@ -5,9 +5,18 @@ package org.example;
 
 
 public class Dev {
+    Laptop laptop;
+
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop; // setter injection configured in spring.xml
+//    }
+
+    public Dev(Laptop laptop){
+        this.laptop=laptop; // constructor injection
+    }
 
     public void depInject(){
-
+        laptop.compile();
         System.out.println("This is an example of Spring Project");
     }
 }
